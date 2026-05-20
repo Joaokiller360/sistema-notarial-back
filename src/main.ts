0efa9c7a1f07494d8f304746541badf4b4734353
Joaokiller360 <joao1.joao3@gmail.com>
2026-05-20 09:48:39 -0500

    error imagen news

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from "@nestjs/config";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import helmet from "helmet";
-import * as compression from "compression";
+import compression from "compression";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 import { PrismaExceptionFilter } from "./common/filters/prisma-exception.filter";
